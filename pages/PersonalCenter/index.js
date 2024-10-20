5ea08d2854efd6187e671cf95112e492ca50a7d7
valarBetter <3096271350@qq.com>
2024-10-20 19:34:25 +0800

    个人中心页面代码，及子页面个人信息页代码

--- pages/PersonalCenter/index.js
+++ pages/PersonalCenter/index.js
@@ -1,66 +1,41 @@
 // pages/PersonalCenter/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  backEvent: function() {
+    console.log('点击了退出');
+    wx.reLaunch({
+      url: '/pages/login/login/login',
+      fail: function(err) {
+        console.error('跳转失败:', err);  // 输出跳转失败的错误信息
+      }
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  gotoPersonalMessage: function() {
+    console.log('点击了个人信息');
+    wx.navigateTo({
+      url: '/pages/PersonalCenter/personalMessage/index',
+      fail: function(err) {
+        console.error('跳转失败:', err);  // 输出跳转失败的错误信息
+      }
+    });
+  },
+  gotoCourseList: function() {
+    console.log('点击了课程表');
+    wx.navigateTo({
+      url: '/pages/PersonalCenter/courseList/index',
+      fail: function(err) {
+        console.error('跳转失败:', err);  // 输出跳转失败的错误信息
+      }
+    });
+  },
+  gotoGradeSearch: function() {
+    console.log('点击了成绩查询');
+    wx.navigateTo({
+      url: '/pages/PersonalCenter/gradeSearch/index',
+      fail: function(err) {
+        console.error('跳转失败:', err);  // 输出跳转失败的错误信息
+      }
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
