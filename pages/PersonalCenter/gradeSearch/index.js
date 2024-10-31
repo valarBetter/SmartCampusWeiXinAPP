87afd6b13848010764ae19661eb9ec08c9812f9b
valarBetter <3096271350@qq.com>
2024-10-31 09:22:40 +0800

    mock数据

--- pages/PersonalCenter/gradeSearch/index.js
+++ pages/PersonalCenter/gradeSearch/index.js
@@ -7,14 +7,47 @@ Page({
       {name: "2025-2023下学期" ,id: 2},
       {name: "2026-2023上学期" ,id: 3}],
       curBrandName:"全部" ,
+      semesters: [] // 新增属性
     },
-
+    
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    const userId = '你的用户ID'; // 替换为实际用户ID
+    const semester = this.data.curBrandName !== '全部' ? this.data.curBrandName : ''; // 只有在不选择“全部”时传递
+  
+    wx.request({
+      url: 'https://example.com/mockGradeSearch',
+      method: 'GET',
+      data: {
+        userId: userId,
+        semester: semester
+      },
+      success: (res) => {
+        console.log(res.data); // 打印返回的 data 对象
+        console.log(res.status);
+        if (res.data.userId==="123456") {
+          this.setData({
+            semesters: res.data.semesters // 设置成绩数据
+          });
+        } else {
+          wx.showToast({
+            title: '数据加载失败11',
+            icon: 'none'
+          });
+        }
+      },
+      
+      fail: () => {
+        wx.showToast({
+          title: '网络错误',
+          icon: 'none'
+        });
+      }
+    });
   },
+  
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -29,7 +62,13 @@ Page({
   onShow() {
 
   },
-
+  changeBrand(e) {
+    this.setData({
+      curBrandName: e.detail.select // 更新当前学期
+    });
+    this.onLoad(); // 重新加载数据
+  },
+  
   /**
    * 生命周期函数--监听页面隐藏
    */
